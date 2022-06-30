b05a1359995c9b85bab3f183e4fffea0915f38b7
渔知 <wangzixiao.wzx@alibaba-inc.com>
2022-06-30 20:35:22 +0800

    <init>[shiro]: 项目初始化

---
+++ src/main/java/com/yuzhi/shiro/shiro_paly_ground/ShiroPalyGroundApplication.java
@@ -0,0 +1,13 @@
+package com.yuzhi.shiro.shiro_paly_ground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroPalyGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroPalyGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuzhi/shiro/shiro_paly_ground/ShiroPalyGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzhi.shiro.shiro_paly_ground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroPalyGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
